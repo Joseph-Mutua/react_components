2897c8d1ea9b1d1c80a757a47b188420bc79d905
Joseph Mutua <Mutuaj793@gmail.com>
2020-11-10 17:36:38 +0300

    Passing and receiving props in components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
       {" "}
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:50PM"
+        content="I love Myself"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 5:06PM"
+        content="The Climb Back"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 9:06PM"
+        content="Whats the need"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
